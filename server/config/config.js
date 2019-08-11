8d36839457a776c8d8d6491c66c6feba958201db
Miguel <miguelbarahona00@gmail.com>
2019-08-11 16:14:37 -0500

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlBD;
 if (process.env.NODE_ENV == 'dev') {
     urlBD = 'mongodb://localhost:27017/cafe';
 } else {
-    urlBD = 'mongodb+srv://admin_atlas:45jN3ANk03Lqo1RU@cluster0-jspuf.mongodb.net/cafe';
+    urlBD = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlBD;
